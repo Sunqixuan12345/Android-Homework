15f584a9000798d84b614697e0c07c3eb9afd506
Sunqixuan12345 <1057047757@qq.com>
2020-10-16 17:06:34 +0800

    作业
    作业

---
+++ homework0002_1/app/src/main/java/com/example/homework0002_1/Marquee.java
@@ -0,0 +1,29 @@
+package com.example.homework0002_1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class Marquee extends AppCompatTextView {
+    public Marquee(Context context) {
+        super(context);
+    }
+
+    public Marquee(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Marquee(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public boolean isFocused() {
+        return true;
+    }
+}
+
+
+
+
+
